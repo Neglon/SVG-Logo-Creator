b119c55d49ea9e1886ab0b149a842f86dd1ca1c0
Neglon <thomasfneylon@gmail.com>
2024-02-06 16:46:18 -0500

    first commit
    set up basic file structure, index.js to run the cli.js, created the cli.js with the questions needed for the project, installed node, jest and inquirer, set up git ignore

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # SVG-Logo-Creator
+
+
+
+
+
+
+
+using validate and testing in inquirer
+https://stackoverflow.com/questions/57321266/how-to-test-inquirer-validation
---
+++ index.js
@@ -0,0 +1,7 @@
+const CLI = require('./lib/cli.js');
+
+//creates a new instance of the CLI class
+const cli = new CLI();
+
+// runs the CLI when node index.js is run
+cli.run();
